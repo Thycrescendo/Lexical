ff9bf70e18367719c07238c83bcc3e67eadf6eb8
thycrescendo <dmicholn@gmail.com>
2024-11-23 03:44:16 +0100

    reset the wolf alpha

--- contracts/deployedContracts.ts
+++ contracts/deployedContracts.ts
@@ -5,3 +5,5 @@
 import { GenericContractsDeclaration } from "../utils/scaffold-eth/contract";
 
 const deployedContracts = {} as const;
+
+export default deployedContracts satisfies GenericContractsDeclaration;
