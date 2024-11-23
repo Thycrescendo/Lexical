8ec5fa4ca331586673445ed3fb8fd13170007bd5
thycrescendo <dmicholn@gmail.com>
2024-11-23 03:43:32 +0100

    updated the deployment

--- contracts/deployedContracts.ts
+++ contracts/deployedContracts.ts
@@ -5,5 +5,3 @@
 import { GenericContractsDeclaration } from "../utils/scaffold-eth/contract";
 
 const deployedContracts = {} as const;
-
-export default deployedContracts satisfies GenericContractsDeclaration;
